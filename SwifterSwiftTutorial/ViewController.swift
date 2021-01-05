21cb7377c939f40cde07683dfdeb87cfec930246
Alex Nagy <therebeloper@gmail.com>
2021-01-05 13:54:03 +0200

    Initial Commit

---
+++ SwifterSwiftTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwifterSwiftTutorial
+//
+//  Created by Alex Nagy on 05.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
